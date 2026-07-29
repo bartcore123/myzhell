6f76d7f4db412804ec650bbe7e61cce47ce1c14d
Bart Houweling <barthouw@gmail.com>
2026-07-29 17:19:30 +0200

    first draft of install script. I'd be surprised if it works. best don't use.

--- install.zsh
+++ install.zsh
@@ -1,2 +1,20 @@
 #!/bin/zsh
 
+cd ${MYSHELL:-$HOME/myshell}
+hash -d M=$PWD
+source ./rc/zshrc/nd
+source ./rc/zshrc/opt
+
+cd ~
+
+[[ .zshrc ]] && mv -i .zshrc .zshrc.old
+[[ .nanorc ]] && mv -i .nanorc .nanorc.old
+[[ (|.)zsh-syntax-highlighting ]] || git clone https://github.com/zsh-users/zsh-syntax-highlighting .zsh-syntax-highlighting
+[[ $ZSH ]] || sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+[[ .zshrc ]] && mv .zshrc .zshrc.omz.bak
+
+ln -s ~rc/.nanorc
+ln -s ~rc/.zshrc
+
+source .zshrc
